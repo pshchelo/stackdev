f191e81496ebdf3cab56f27c7ea322dde11d929b
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2014-09-08 22:47:09 +0000

    WIP further improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 # install personal dev dependencies and sets up dev environment
 
-# install some dependencies
-sudo apt-get install mc htop ctags git
+# install some goodies and dependencies
 DOTFILES="$HOME/dotfiles"
 git clone git@github.com:pshchelo/dotfiles.git $DOTFILES
 
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local/bin"
+sudo apt-get install mc htop ctags
 # create links
 ln -s "$DOTFILES/ack/ackrc" "$HOME/.ackrc"
 ln -s "$DOTFILES/git/gitignore_global" "$HOME/.gitignore_global"
@@ -14,22 +16,16 @@ ln -s "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
 ln -s "$DOTFILES/shell/tmux.conf" "$HOME/.tmux.conf"
 ln -s "$DOTFILES/shell/bash_aliases" "$HOME/.bash_aliases"
 ln -s "$DOTFILES/colors/dircolors-solarized" "$HOME/.dircolors"
-ln -s "$DOTFILES/powerline" "$HOME/.config/powerline"
 mv "$HOME/.profile" "$HOME/.profile-original"
 mv "$HOME/.bashrc" "$HOME/.bashrc-original"
 ln -s "$DOTFILES/shell/bashrc" "$HOME/.bashrc"
 ln -s "$DOTFILES/environment/profile" "$HOME/.profile"
-source $HOME/.bashrc
-
-ack_bin = "$HOME/.local/bin/ack"
-wget http://beyondgrep.com/ack-2.14-single-file -O $ack_bin && chmod 0755 $ack_bin
-ln -s "$DOTFILES/tig-2.0.3_x64" "$HOME/.local/bin/tig"
+ln -s "$DOTFILES/powerline" "$HOME/.config/powerline"
+#source $HOME/.bashrc
 
 # create and edit gitconfig
 cp "$DOTFILES/git/gitconfig" "$HOME/.gitconfig"
 sed -i 's/shchelokovskyy@gmail/pshchelokovskyy@mirantis/' "$HOME/.gitconfig"
-# following one is for the config to work on older git
-#sed -i 's/default = simple//' "$HOME/.gitconfig"
 
 #mkdir -p "$HOME/.config/mc"
 #ln -s "$DOTFILES/colors/mc-solarized" "$HOME/.config/mc/mc-solarized"
@@ -37,10 +33,16 @@ sed -i 's/shchelokovskyy@gmail/pshchelokovskyy@mirantis/' "$HOME/.gitconfig"
 # install Python packages in local user location
 pip install --user -r dev-requirements.txt
 
+# download and install Ack standalone script
+curl http://beyondgrep.com/ack-2.14-single-file > $ack_bin && chmod 0755 $ack_bin
+chmod 0755 "$HOME/.local/bin/ack"
+# create link to Tig binary
+ln -s "$DOTFILES/bin/tig-2.0.3_x64" "$HOME/.local/bin/tig"
+
 # create ipython profile to hold custom settings
-"$HOME/.local/bin/ipython" profile create
-rm "$HOME/.ipython/profile_default/ipython_config.py"
-ln -s "$DOTFILES/ipython/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
+#"$HOME/.local/bin/ipython" profile create
+#rm "$HOME/.ipython/profile_default/ipython_config.py"
+#ln -s "$DOTFILES/ipython/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
 
 # prepare Vundle to later install ViM plugins
 mkdir -p "$HOME/.vim/bundle"
