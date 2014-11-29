cfdb553b4c1eefe8f348243bdcd301ee8388e4f2
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2014-11-29 11:00:39 +0200

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,8 @@
 DOTFILES="$HOME/dotfiles"
 git clone git@github.com:pshchelo/dotfiles.git $DOTFILES
 
-./$DOTFILES/bootstrap-main.sh
+cd $DOTFILES
+sh bootstrap-main.sh
 
 # create and edit gitconfig
 cp "$DOTFILES/git/gitconfig" "$HOME/.gitconfig"
