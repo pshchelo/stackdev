0e9f6eefc3793195a738874672464d21ded27f7c
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2014-12-01 15:32:15 +0000

    Improve stack-resume script

--- scripts/resume-stack.sh
+++ scripts/resume-stack.sh
@@ -1,11 +1,13 @@
-# enable ip forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
+# enable ip forwarding (looks like devstack is already doing it itself)
+#sudo sysctl -w net.ipv4.ip_forward=1
 # reset bridges
+# VALUES ARE FOR DEFAULT DEVSTACK FLOATING IP RANGE!
 sudo ip addr flush br-ex
 sudo ip addr add 172.24.4.1/24 dev br-ex
 sudo ip link set br-ex up
-sudo route add -net 10.0.0.0/24 gw 172.24.4.1
-# set iptables NAT
+# Not really needed for Neutron (may be Nova-network?)
+#sudo route add -net 10.0.0.0/24 gw 172.24.4.1
+# set iptables NAT so that VMs in principle can have internet access
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 # attach stack-volumes
 sudo losetup /dev/loop0 /opt/stack/data/stack-volumes-lvmdriver-1-backing-file
