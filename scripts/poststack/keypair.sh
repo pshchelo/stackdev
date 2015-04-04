88af969137732b40dd8e6df814e7df99e779b353
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2015-04-04 20:56:24 +0000

    Use my own key for VMs

--- scripts/poststack/keypair.sh
+++ scripts/poststack/keypair.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 . /opt/stack/devstack/accrc/demo/demo
-nova keypair-add demo > $HOME/demo.pem
-chmod 600 $HOME/demo.pem
+nova keypair-add demo --pub_key $HOME/.ssh/git_rsa.pub
 nova keypair-list
