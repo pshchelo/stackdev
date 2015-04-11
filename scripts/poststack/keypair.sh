79644fe6c51e4ed448338daa396c03dd453da269
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2015-04-11 18:37:42 +0000

    Move most poststack code to makefile
    and remove those shell scripts.

--- scripts/poststack/keypair.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-. /opt/stack/devstack/accrc/demo/demo
-nova keypair-add demo --pub_key $HOME/.ssh/git_rsa.pub
-nova keypair-list
--- scripts/poststack/rncirros.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env bash
-. /opt/stack/devstack/accrc/admin/admin
-
-name=$(glance image-list | grep -o "cirros-.*-disk")
-
-glance image-update $name --name cirros --property description=$name
