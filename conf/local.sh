6a2127476e393599bad4fee1894a2aec85caed11
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2016-05-16 13:48:51 +0300

    Unify credentials handling in local.sh

--- conf/local.sh
+++ conf/local.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
-CREDS=/opt/stack/devstack/openrc
-OSCLI="openstack --os-cloud devstack"
-OSCLI_ADMIN="openstack --os-cloud devstack-admin"
-NCLI="neutron --os-cloud devstack"
-NCLI_ADMIN="neutron --os-cloud devstack-admin"
-CATALOG=`${OSCLI_ADMIN} catalog list -f value -c Name`
+DEMO="--os-cloud devstack"
+ADMIN="--os-cloud devstack-admin"
+CATALOG=`openstack ${ADMIN} catalog list -f value -c Name`
 
 function has_services {
     services=$@
@@ -27,7 +24,7 @@ function allow_wan {
 function add_keypair {
     if has_services nova; then
         echo "Adding demo keypair..."
-        ${OSCLI} keypair create demo --public-key $HOME/.ssh/git_rsa.pub
+        openstack ${DEMO} keypair create demo --public-key $HOME/.ssh/git_rsa.pub
     else
         echo "Nova is not installed, skip adding keypair"
     fi
@@ -38,16 +35,15 @@ function add_dns {
         echo "Adding Google DNS to demo tenant private subnets..."
         # NOTE: openstackclient has no support for neutron subnet CLI for now,
         #       resorting to neutronclient
-        #source $CREDS demo demo
         dnsserver4=8.8.8.8
-        subnet4=$($NCLI subnet-list -f value | grep private-subnet | grep -v ipv6-private-subnet | awk '{print $1}')
-        $NCLI subnet-update $subnet4 --dns-nameserver $dnsserver4
-        $NCLI subnet-show $subnet4
-        subnet6=$($NCLI subnet-list -f value | grep ipv6-private-subnet | awk '{print $1}')
+        subnet4=$(neutron ${DEMO} subnet-list -f value | grep private-subnet | grep -v ipv6-private-subnet | awk '{print $1}')
+        neutron ${DEMO} subnet-update $subnet4 --dns-nameserver $dnsserver4
+        neutron ${DEMO} subnet-show $subnet4
+        subnet6=$(neutron ${DEMO} subnet-list -f value | grep ipv6-private-subnet | awk '{print $1}')
         if [ -n "${subnet6}" ]; then
             dnsserver6="2001:4860:4860::8888"
-            $NCLI subnet-update $subnet6 --dns-nameserver $dnsserver6
-            $NCLI subnet-show $subnet6
+            neutron ${DEMO} subnet-update $subnet6 --dns-nameserver $dnsserver6
+            neutron ${DEMO} subnet-show $subnet6
         fi
     else
         echo "Neutron is not installed, skip adding DNS."
@@ -59,16 +55,14 @@ function add_heat_net {
         echo "Adding subnet for heat tests..."
         # NOTE: openstackclient has no support for neutron subnet, port and routers CLI for now,
         #       resorting to neutronclient
-        #source $CREDS admin admin
-        PUB_SUBNET_ID=$($NCLI_ADMIN subnet-list | grep ' public-subnet ' | awk '{split($0,a,"|"); print a[2]}')
-        ROUTER_GW_IP=$($NCLI_ADMIN port-list -c fixed_ips -c device_owner | grep router_gateway | awk -F '"' -v subnet_id="${PUB_SUBNET_ID//[[:space:]]/}" '$4 == subnet_id { print $8; }')
+        PUB_SUBNET_ID=$(neutron ${ADMIN} subnet-list | grep ' public-subnet ' | awk '{split($0,a,"|"); print a[2]}')
+        ROUTER_GW_IP=$(neutron ${ADMIN} port-list -c fixed_ips -c device_owner | grep router_gateway | awk -F '"' -v subnet_id="${PUB_SUBNET_ID//[[:space:]]/}" '$4 == subnet_id { print $8; }')
 
         # create a heat specific private network (default 'private' network has ipv6 subnet)
-        #source $CREDS demo demo
         HEAT_PRIVATE_SUBNET_CIDR=10.0.5.0/24
-        $NCLI net-create heat-net
-        $NCLI subnet-create --name heat-subnet heat-net $HEAT_PRIVATE_SUBNET_CIDR
-        $NCLI router-interface-add router1 heat-subnet
+        neutron ${DEMO} net-create heat-net
+        neutron ${DEMO} subnet-create --name heat-subnet heat-net $HEAT_PRIVATE_SUBNET_CIDR
+        neutron ${DEMO} router-interface-add router1 heat-subnet
         sudo route add -net $HEAT_PRIVATE_SUBNET_CIDR gw $ROUTER_GW_IP
     else
         echo "Heat or Neutron not installed, skip adding Heat test network."
@@ -81,9 +75,9 @@ function secgroup {
         # NOTE: openstackclient currently manages secgroups via nova client,
         #       which understands only ingress rules,
         #       and this is exactly what I need 
-        ${OSCLI} security group rule list default -f value -c ID | xargs -L1 ${OSCLI} security group rule delete
-        ${OSCLI} security group rule create default --proto icmp --src-ip "0.0.0.0/0"
-        ${OSCLI} security group rule create default --proto tcp --src-ip "0.0.0.0/0" --dst-port 22
+        openstack ${DEMO} security group rule list default -f value -c ID | xargs -L1 openstack ${DEMO} security group rule delete
+        openstack ${DEMO} security group rule create default --proto icmp --src-ip "0.0.0.0/0"
+        openstack ${DEMO} security group rule create default --proto tcp --src-ip "0.0.0.0/0" --dst-port 22
     else
         echo "Neutron is not installed, skip modifying default security groups."
     fi
@@ -92,10 +86,10 @@ function secgroup {
 function rename_cirros {
     if has_services glance; then
         echo "Renaming cirros image..."
-        read -r -a image <<< `${OSCLI_ADMIN} image list -f value -c ID -c Name | grep "cirros-.*-disk"`
+        read -r -a image <<< `openstack ${ADMIN} image list -f value -c ID -c Name | grep "cirros-.*-disk"`
         if [ -n "image[0]" ]; then
-            ${OSCLI_ADMIN} image set ${image[0]} --name cirros --property description=${image[1]}
-            ${OSCLI_ADMIN} image show ${image[0]}
+            openstack ${ADMIN} image set ${image[0]} --name cirros --property description=${image[1]}
+            openstack ${ADMIN} image show ${image[0]}
         fi
     else
         echo "Glance is not installed, skip renaming Cirros image."
@@ -107,7 +101,7 @@ function add_awslb_image {
         echo "Uploading Fedora 21 cloud image to glance"
         AWS_LB_IMAGE_NAME=Fedora-Cloud-Base-20141203-21.x86_64
         AWS_LB_IMAGE_URL="http://download.fedoraproject.org/pub/fedora/linux/releases/21/Cloud/Images/x86_64/${AWS_LB_IMAGE_NAME}.qcow2"
-        curl -L ${AWS_LB_IMAGE_URL} | ${OSCLI_ADMIN} image create --public --disk-format qcow2  --container-format bare ${AWS_LB_IMAGE_NAME}
+        curl -L ${AWS_LB_IMAGE_URL} | openstack ${ADMIN} image create --public --disk-format qcow2  --container-format bare ${AWS_LB_IMAGE_NAME}
     else
         echo "Heat or Glance not installed, skip adding base image for AWS LoadBalancer."
     fi
