2dc59f08e5ce4ce153ee4bec1de98711835a9168
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2016-11-18 15:48:10 +0200

    Impove base virtual image builder script

--- scripts/build-virt-base.sh
+++ scripts/build-virt-base.sh
@@ -4,12 +4,15 @@ tmpkey=/tmp/ssh_tmp_pub_key_file
 
 wget https://github.com/"${USER}".keys -O $tmpkey
 
+IMAGE_BASE=${1:-'ubuntu'}
+RELEASE=${2:-'xenial'}
+SIZE=${3:-'50'}
 export DIB_DEV_USER_USERNAME="${USER}"
 export DIB_DEV_USER_PWDLESS_SUDO=true
 export DIB_DEV_USER_AUTHORIZED_KEYS=$tmpkey
 export DIB_DEV_USER_SHELL='/bin/bash'
-export DIB_RELEASE='xenial'
+export DIB_RELEASE=${RELEASE}
 
-disk-image-create ubuntu vm growroot cloud-init-nocloud devuser -o "${USER}"-virt-base -a amd64 --image-size 50
+disk-image-create ${IMAGE_BASE} vm growroot cloud-init-nocloud devuser -o "${USER}-${IMAGE_BASE}-${RELEASE}"-virt-base-${SIZE}G -a amd64 --image-size ${SIZE}
 
 rm $tmpkey
