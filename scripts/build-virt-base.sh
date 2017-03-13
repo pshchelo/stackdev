213540a19df718cee1289e93e88af7bf330cfa1a
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2017-03-13 14:03:22 +0200

    Revert some image building script changes
    Have the output file name always be a default one.

--- scripts/build-virt-base.sh
+++ scripts/build-virt-base.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 tmpkey=/tmp/ssh_tmp_pub_key_file
 
+# relies on building user name to be present as account on GitHub
 wget https://github.com/"${USER}".keys -O $tmpkey
 
-IMAGE_BASE=${2:-'ubuntu'}
-RELEASE=${3:-'xenial'}
-SIZE=${4:-'50'}
-OUT_NAME=${1:-"${USER}-${IMAGE_BASE}-${RELEASE}-virt-base-${SIZE}G"}
+IMAGE_BASE=${1:-'ubuntu'}
+RELEASE=${2:-'xenial'}
+SIZE=${3:-'50'}
 
 export DIB_DEV_USER_USERNAME="${USER}"
 export DIB_DEV_USER_PWDLESS_SUDO=true
@@ -17,6 +17,6 @@ export DIB_RELEASE=${RELEASE}
 disk-image-create \
     ${IMAGE_BASE} vm growroot cloud-init-nocloud devuser \
     -a amd64 --image-size ${SIZE} \
-    -o "${OUT_NAME}"
+    -o "${USER}-${IMAGE_BASE}-${RELEASE}-virt-base-${SIZE}G"
 
 rm $tmpkey
