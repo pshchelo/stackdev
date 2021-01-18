7fd971a1e13e3da0fa944f2252d99867009251ca
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2021-01-18 20:37:15 +0200

    Add script to find servers out of Heat stacks

---
+++ scripts/solitary_servers.py
@@ -0,0 +1,27 @@
+import openstack
+
+cloud = openstack.connect()
+
+def get_servers_in_stacks():
+    heat = cloud.orchestration
+    servers_in_stacks = []
+    for stack in heat.stacks():
+        server_resources = heat.get(
+            f"/stacks/{stack.name}/{stack.id}/resources",
+            params={"type": "OS::Nova::Server", "nested_depth": "999"}
+        ).json()
+        servers_in_stacks.extend(
+            [r["physical_resource_id"] for r in server_resources["resources"]])
+    return servers_in_stacks
+
+
+def get_solitary_servers():
+    nova = cloud.compute
+    servers_in_stacks = get_servers_in_stacks()
+    return [s for s in nova.servers() if s.id not in servers_in_stacks]
+
+if __name__ == "__main__":
+    for server in get_solitary_servers():
+        print(server.id, server.name)
+
+
