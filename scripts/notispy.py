367dd4b2f7d360fdea3f33ad86b6cd36b32cf2d4
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2021-08-20 12:12:06 +0000

    Add argument processing to notispy script
    main thing left is specifying pool still somehow does not work...

--- scripts/notispy.py
+++ scripts/notispy.py
@@ -1,12 +1,18 @@
-# import eventlet
-# eventlet.monkey_patch()
+#!/usr/bin/env python3
 
+import argparse
 import logging
 
 from oslo_config import cfg
 import oslo_messaging
 
-logging.basicConfig(level=logging.INFO)
+"""
+https://docs.openstack.org/oslo.messaging/latest/reference/notification_listener.html
+"""
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+)
 LOG = logging.getLogger("notispy")
 
 
@@ -16,20 +22,49 @@ class NotificationEndpoint(object):
                  ctxt, publisher_id, event_type, payload, metadata)
 
 
-transport = oslo_messaging.get_notification_transport(
-    cfg.CONF, url="rabbit://stackrabbit:rabbit@192.168.100.225:5672")
-targets = [
-    oslo_messaging.Target(topic='notifications'),
-]
-endpoints = [NotificationEndpoint()]
-pool = "notispy"
-server = oslo_messaging.get_notification_listener(
-    transport,
-    targets,
-    endpoints,
-    # executor='eventlet',
-    # pool=pool,
-)
+def main():
+    parser = argparse.ArgumentParser("notispy")
+    parser.add_argument("--config")
+    parser.add_argument("--url")
+    parser.add_argument("--topic", default="notifications")
+    parser.add_argument("--pool", default="notispy")
+    parser.add_argument("--consume", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
 
-server.start()
-server.wait()
+    if args.debug:
+        LOG.setLevel(logging.DEBUG)
+    else:
+        LOG.setLevel(logging.INFO)
+
+    if args.config:
+        cfg.CONF(["--config-file", args.config])
+
+
+    cfg.CONF.heartbeat_interval = 5
+    cfg.CONF.prog = parser.prog
+    cfg.CONF.project = parser.prog
+
+
+    transport = oslo_messaging.get_notification_transport(
+        cfg.CONF, url=args.url)
+    targets = [
+        oslo_messaging.Target(topic=args.topic),
+    ]
+    endpoints = [
+        NotificationEndpoint()
+    ]
+
+    server = oslo_messaging.get_notification_listener(
+        transport,
+        targets,
+        endpoints,
+        pool=None if args.consume else args.pool,
+    )
+
+    LOG.debug("Starting notispy server...")
+    server.start()
+    server.wait()
+
+if __name__ == "__main__":
+    main()
