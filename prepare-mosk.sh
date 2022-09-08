f5ace3e3fb5a437c32505a901a6a6742b580311f
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2022-09-08 18:02:55 +0300

    Update prepare-mosk to equal to clouds.yaml

--- prepare-mosk.sh
+++ prepare-mosk.sh
@@ -1,6 +1,6 @@
 kopenstack flavor create m1.nano --ram 128 --disk 1 --vcpu 1
-kopenstack user create demo --password admin --domain default
-kopenstack user create alt_demo --password admin --domain default
-kopenstack project create demo --domain default
+kopenstack user create --or-show demo --password admin --domain default
+kopenstack user create alt-demo --or-show --password alt-demo --domain default
+kopenstack project create --or-show demo --domain default
 kopenstack role add member --user demo --user-domain default --project demo --project-domain default
-kopenstack role add member --user alt_demo --user-domain default --project demo --project-domain default
+kopenstack role add member --user alt-demo --user-domain default --project demo --project-domain default
