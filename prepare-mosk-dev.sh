144b8e825c6f279fac81ece454fff1667ba970bf
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-01-18 15:15:16 +0200

    Rewrite prepare mosk script
    do not rely on any custom scripts or aliases, define them all in place.

--- prepare-mosk-dev.sh
+++ prepare-mosk-dev.sh
@@ -1,25 +1,35 @@
-echo "Admin password is: $(kubectl -n openstack get secret keystone-os-clouds -ojsonpath="{.data['clouds\.yaml']}" | base64 -d | yq -r .clouds.admin.auth.password)"
+ko="kubectl -n openstack exec -ti deploy/keystone-client -c keystone-client -- openstack"
+
+admin_pass=$(kubectl -n openstack get secret keystone-keystone-admin -ojsonpath='{.data.password}' | base64 -d)
+echo "Admin password is: $admin_pass"
+echo "Writing secure.yaml file for mosk-dev-admin cloud"
+cat > secure.yaml << EOF
+clouds:
+  mosk-dev-admin:
+    auth:
+      password: $admin_pass
+EOF
 
 # Need this role to use Barbican and encrypted storage for instances and volumes
-kopenstack role create --or-show creator
+$ko role create --or-show creator
 # sandbox project to use w/o admin privileges
-kopenstack project create demo --domain Default
+$ko project create demo --domain Default
 
 # sandbox user to use w/o admin priveleges
-kopenstack user create demo --domain Default --password demo
-kopenstack role add member --user demo --user-domain Default --project demo --project-domain Default
-kopenstack role add creator --user demo --user-domain Default --project demo --project-domain Default
+$ko user create demo --domain Default --password demo
+$ko role add member --user demo --user-domain Default --project demo --project-domain Default
+$ko role add creator --user demo --user-domain Default --project demo --project-domain Default
 
 # another sandbox user to use w/o admin privileges
-kopenstack user create alt-demo --domain Default --password alt-demo
-kopenstack role add member --user alt-demo --user-domain Default --project demo --project-domain Default
+$ko user create alt-demo --domain Default --password alt-demo
+$ko role add member --user alt-demo --user-domain Default --project demo --project-domain Default
 
 # Minimal flavor for cirros
-kopenstack flavor create m1.nano --ram 128 --disk 1 --vcpus 1
+$ko flavor create m1.nano --ram 128 --disk 1 --vcpus 1
 
 
-# TODO: change to kopenstack calls using 'demo' project id
-# openstack keypair create demo --public-key ~/.ssh/pub/aio_rsa.pub 
+# TODO: change to ko calls using 'demo' project id
+# openstack keypair create demo --public-key ~/.ssh/pub/aio_rsa.pub
 # openstack network create demo
 # openstack subnet create demo --network demo --subnet-range 10.20.30.0/24
 # openstack router create demo --external-gateway public
