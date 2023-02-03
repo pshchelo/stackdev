13410b9e0ad8c42fea58358a6afe3642cbbc9408
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-02-03 11:53:30 +0200

    Add script to fecth mosk-dev env kubeconfig

---
+++ fetch-mosk-dev-config.sh
@@ -0,0 +1,4 @@
+server=$1
+key=${2:~/.ssh/aio_rsa}
+aiossh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $key"
+$aiossh -l ubuntu $server -- sudo cat /root/.kube/config | sed 's/certificate-authority-data.*$/insecure-skip-tls-verify: true/' | sed "s/server: https.*/server: https:\/\/$server:6443/"
