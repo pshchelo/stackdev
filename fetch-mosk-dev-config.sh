4fc1f1b79db11931fa4fb05d130331269815ec85
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-02-15 15:12:25 +0200

    Fix fetch mosk-dev kubeconfig script

--- fetch-mosk-dev-config.sh
+++ fetch-mosk-dev-config.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 server=$1
-key=${2:~/.ssh/aio_rsa}
+key=${2:-~/.ssh/aio_rsa}
 aiossh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $key"
 $aiossh -l ubuntu $server -- sudo cat /root/.kube/config | sed 's/certificate-authority-data.*$/insecure-skip-tls-verify: true/' | sed "s/server: https.*/server: https:\/\/$server:6443/"
