2383f6e7e54a80ea4c39af99a0241344793e7393
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-07-01 14:53:54 +0300

    Pre-create and use admin user for mosk-dev envs

--- prepare-mosk-dev.sh
+++ prepare-mosk-dev.sh
@@ -1,22 +1,18 @@
 ko="kubectl -n openstack exec -ti deploy/keystone-client -c keystone-client -- openstack"
 
-admin_name=$(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d | yq -r .clouds.admin.auth.username)
-admin_pass=$(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d | yq -r .clouds.admin.auth.password)
-echo "Admin username is: $admin_name"
-echo "Admin password is: $admin_pass"
-echo "Writing secure.yaml file for mosk-dev-admin cloud"
-cat > secure.yaml << EOF
-clouds:
-  mosk-dev-admin:
-    auth:
-      username: $admin_name
-      password: $admin_pass
-EOF
-
 # Need this role to use Barbican and encrypted storage for instances and volumes
 $ko role create --or-show creator
 # sandbox project to use w/o admin privileges
 $ko project create demo --domain Default
+
+# separate admin user - not auto-rotated, with stable name and password,
+# to be used from outside
+$ko user create superadmin --domain Default --password superadmin
+$ko role add admin --user superadmin --user-domain Default --project admin --project-domain Default
+$ko role add admin --user superadmin --user-domain Default --domain Default
+$ko role add admin --user superadmin --user-domain Default --system all
+# custom MOSK role
+$ko role add global-secret-decoder --user superadmin --user-domain Default --project admin --project-domain Default
 
 # sandbox user to use w/o admin priveleges
 $ko user create demo --domain Default --password demo
@@ -29,7 +25,6 @@ $ko role add member --user alt-demo --user-domain Default --project demo --proje
 
 # Minimal flavor for cirros
 $ko flavor create m1.nano --ram 128 --disk 1 --vcpus 1
-
 
 # TODO: change to ko calls using 'demo' project id
 # openstack keypair create demo --public-key ~/.ssh/pub/aio_rsa.pub
