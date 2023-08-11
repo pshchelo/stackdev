2879b2814ba07d8550fa4ac941ce454096de091d
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-08-11 16:23:19 +0300

    Flake8 for project-resources script

--- scripts/project-resources.py
+++ scripts/project-resources.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import openstack, sys
+import sys
+
+import openstack
 
 sdk_kwargs = {}
 if len(sys.argv) > 1:
@@ -8,8 +10,12 @@ if len(sys.argv) > 1:
 cloud = openstack.connect(**sdk_kwargs)
 
 nova_limits = cloud.compute.get_limits()
-available_cores = nova_limits.absolute.total_cores - nova_limits.absolute.total_cores_used
-available_ram = nova_limits.absolute.total_ram - nova_limits.absolute.total_ram_used
+available_cores = (
+    nova_limits.absolute.total_cores - nova_limits.absolute.total_cores_used
+)
+available_ram = (
+    nova_limits.absolute.total_ram - nova_limits.absolute.total_ram_used
+)
 
 print(f"Available Cores: {available_cores}")
 print(f"Available RAM:   {available_ram // 1024} GB")
