f53fbdbf4329b8f6ed201348381544c3a1d1b511
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-08-14 12:30:32 +0300

    Improve prepare-mosk-dev script

--- prepare-mosk-dev.sh
+++ prepare-mosk-dev.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 ko="kubectl -n openstack exec -ti deploy/keystone-client -c keystone-client -- openstack"
 
 # Need this role to use Barbican and encrypted storage for instances and volumes
 $ko role create --or-show creator
 # sandbox project to use w/o admin privileges
-demo_project=$($ko project create demo --domain Default)
+demo_project=$($ko project create demo --domain Default -f value -c id --or-show)
 
 # separate admin user - not auto-rotated, with stable name and password,
 # to be used from outside
@@ -16,7 +17,7 @@ $ko role add global-secret-decoder --user superadmin --user-domain Default --pro
 $ko role add global-secret-decoder --user superadmin --user-domain Default --system all
 
 # sandbox user to use w/o admin priveleges
-demo_user=$($ko user create demo --domain Default --password demo)
+demo_user=$($ko user create demo --domain Default --password demo -f value -c id --or-show)
 $ko role add member --user demo --user-domain Default --project demo --project-domain Default
 $ko role add creator --user demo --user-domain Default --project demo --project-domain Default
 
@@ -34,11 +35,12 @@ $ko role add reader --user viewer --user-domain Default --system all
 $ko flavor create m1.nano --ram 128 --disk 1 --vcpus 1
 
 # TODO: needs testing
-#n_id=$(ko network create demo --project $demo_project -f value -c id)
+#n_id=$($ko network create demo --project $demo_project -f value -c id)
 #s_id=$($ko subnet create demo --network $n_id --subnet-range 10.20.30.0/24 --project $demo_project -f value -c id)
 #r_id=$($ko router create demo --external-gateway public --project $demo_project -f value -c id)
 #$ko router add subnet $r_id $s_id --project $demo_project
 #$ko security group create demo --project $demo_project
 #$ko security group rule create demo --ingress --protocol tcp --dst-port 22 --description SSH --project $demo_project
 # FIXME how to pass that to container?
-#$ko keypair create demo --public-key ~/.ssh/pub/aio_rsa.pub --user $demo_user
+#kubectl -n openstack cp ~/.ssh/pub/aio_rsa.pub deploy/keystone-client:/tmp/pubkey
+#$ko keypair create demo --public-key /tmp/pubkey --user $demo_user
