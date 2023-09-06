3de9916ce65ae4eb24742fb68c9ba07f4ccf1aef
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-09-06 15:44:35 +0000

    Improve adapt-tempest-conf script
    fetch all the required files from paused tempest pod
    to be able to run tempest locally

--- tempest/adapt-tempest-conf.sh
+++ tempest/adapt-tempest-conf.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-tempest_conf=${1:-tempest.conf}
+tempest_pod=$(kubectl -n openstack get pod -l application=tempest,component=run-tests -o jsonpath='{.items[*].metadata.name}')
+
+kubectl -n openstack cp -c tempest-run-tests $tempest_pod:/etc/tempest/test-blacklist ./test-blacklist
+
+tempest_conf=${PWD}/tempest.conf
+kubectl -n openstack cp -c tempest-run-tests $tempest_pod:/etc/tempest/tempest.conf $tempest_conf
+
 crudini --del $tempest_conf identity ca_certificates_file
 crudini --set $tempest_conf identity disable_ssl_certificate_validation True
 crudini --set $tempest_conf identity v3_endpoint_type public
 crudini --set $tempest_conf identity uri_v3 https://keystone.it.just.works/v3
-#crudini --set $tempest_conf DEFAULT state_path FIXME
-#crudini --set $tempest_conf load_balancer test_server_path FIXME
-#crudini --set $tempest_conf scenario img_file FIXME
+
+resources_dir=${PWD}/resources
+mkdir -p $resources_dir
+crudini --set $tempest_conf DEFAULT state_path $resources_dir
+
+kubectl -n openstack cp -c tempest-run-tests $tempest_pod:$(crudini --get $tempest_conf scenario img_file) $resources_dir/test.img
+crudini --set $tempest_conf scenario img_file ${PWD}/test.img
+kubectl -n openstack cp -c tempest-run-tests $tempest_pod:$(crudini --get $tempest_conf load_balancer test_server_path) $resources_dir/test_server.bin
+crudini --set $tempest_conf load_balancer test_server_path ${PWD}/test_server.bin
