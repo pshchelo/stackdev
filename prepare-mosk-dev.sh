482977161b4e3086c9c370b7a26c750e9568808d
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-09-29 11:57:14 +0000

    Improve mosk-dev resource creation
    - deal with system scope and reader role not available in Queens
    - create basic project infra (networking, keypair) for admin project too

--- prepare-mosk-dev.sh
+++ prepare-mosk-dev.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -ex
+pod=$(kubectl -n openstack get pod -l application=keystone,component=client -ojsonpath='{.items[0].metadata.name}')
 # create separate admin user - not auto-rotated, with stable name and password, to be used from outside
 # run it from the keystone-client pod using the active admin creds there
 # NOTE: using system scope for Keystone ops as it is safest bet when strict admin / new policies are enforced
-# TODO: discover somehow OpenStack release, as system scope is not present in Queens
-pod=$(kubectl -n openstack get pod -l application=keystone,component=client -ojsonpath='{.items[0].metadata.name}')
 kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system user create superadmin --domain Default --password superadmin --or-show
 kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --project admin --project-domain Default
 kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --domain Default
-kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --system all
+osc_version=$(kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --version | awk '{print $2}' | awk -F '.' '{print$1$2}')
+# openstackclient supports assigning system roles since version 3.16.0/Rocky
+if (( $osc_version >= 316 )); then
+    kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --system all
+fi
 
 # execute the rest locally using separate admin user created above
 # needs access to public OpenStack API of MOSK dev cluster, e.g. sshuttle running
@@ -27,24 +30,28 @@ openstack --os-cloud mosk-dev-admin role add creator --user demo --user-domain D
 # another sandbox user to use w/o admin privileges
 openstack --os-cloud mosk-dev-admin user create alt-demo --domain Default --password alt-demo --or-show
 openstack --os-cloud mosk-dev-admin role add member --user alt-demo --user-domain Default --project demo --project-domain Default
-# readonly user
-openstack --os-cloud mosk-dev-admin user create viewer --domain Default --password viewer
-openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project admin --project-domain Default
-openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project demo --project-domain Default
-openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --system all
+# readonly user - reader role is present since Rocky
+if (( $osc_version >= 316 )); then
+    openstack --os-cloud mosk-dev-admin user create viewer --domain Default --password viewer
+    openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project admin --project-domain Default
+    openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project demo --project-domain Default
+    openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --system all
+fi
 # Minimal flavor for cirros
 openstack --os-cloud mosk-dev-admin flavor create m1.nano --ram 128 --disk 1 --vcpus 1
 
-# network, subnet, router, secgroup for demo project
-openstack --os-cloud mosk-dev-demo network create demo
-openstack --os-cloud mosk-dev-demo subnet create demo --network demo --subnet-range 10.20.30.0/24
-openstack --os-cloud mosk-dev-demo router create demo
-# NOTE: older OpenStack's (Queens etc) do not allow setting external gateway on the router right during creation
-openstack --os-cloud mosk-dev-demo router set demo --external-gateway public
-openstack --os-cloud mosk-dev-demo router add subnet demo demo
-openstack --os-cloud mosk-dev-demo security group create demo
-openstack --os-cloud mosk-dev-demo security group rule create demo --ingress --protocol icmp --description PING
-openstack --os-cloud mosk-dev-demo security group rule create demo --ingress --protocol tcp --dst-port 22 --description SSH
-openstack --os-cloud mosk-dev-demo security group rule create demo --ingress --protocol tcp --dst-port 80 --description HTPP
-# keypair for demo user
-openstack --os-cloud mosk-dev-demo keypair create demo --public-key ${HOME}/.ssh/pub/aio_rsa.pub
+for name in admin demo; do
+    openstack --os-cloud mosk-dev-$name network create $name
+    openstack --os-cloud mosk-dev-$name subnet create $name --network $name --subnet-range 10.20.30.0/24
+    openstack --os-cloud mosk-dev-$name router create $name
+    # NOTE: older OpenStack's (Queens etc) do not allow setting external gateway on the router right during creation
+    openstack --os-cloud mosk-dev-$name router set $name --external-gateway public
+    openstack --os-cloud mosk-dev-$name router add subnet $name $name
+
+    openstack --os-cloud mosk-dev-$name security group create $name
+    openstack --os-cloud mosk-dev-$name security group rule create $name --ingress --protocol icmp --description PING
+    openstack --os-cloud mosk-dev-$name security group rule create $name --ingress --protocol tcp --dst-port 22 --description SSH
+    openstack --os-cloud mosk-dev-$name security group rule create $name --ingress --protocol tcp --dst-port 80 --description HTPP
+
+    openstack --os-cloud mosk-dev-$name keypair create $name --public-key ${HOME}/.ssh/pub/aio_rsa.pub
+done
