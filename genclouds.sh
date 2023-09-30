b914da0798a66e18d3090c70fb7e31fdcd369d31
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-09-30 17:04:40 +0000

    Improve genclouds.sh
    - include region
    - output result to stdout

--- genclouds.sh
+++ genclouds.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 # generate clouds.yaml with admin account for the test MOS env
-mkdir -p ~/.config/openstack
-cat > ~/.config/openstack/clouds.yaml << EOF
-clouds:
-  admin:
+clouds_yaml=$(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d)
+echo "clouds:
+  mosk-dev-admin:
     auth:
       auth_url: https://keystone.it.just.works
-      username: $(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d | yq -r .clouds.admin.auth.username)
-      password: $(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d | yq -r .clouds.admin.auth.password)
+      username: $(echo  "$clouds_yaml" | yq -r .clouds.admin.auth.username)
+      password: $(echo  "$clouds_yaml" | yq -r .clouds.admin.auth.password)
       project_name: admin
       user_domain_name: Default
       project_domain_name: Default
-    insecure: True
-    identity_api_version: 3
-EOF
+    insecure: true
+    interface: public
+    region_name: $(echo  "$clouds_yaml" | yq -r .clouds.admin.region_name)
+    identity_api_version: '3'"
+#EOF
