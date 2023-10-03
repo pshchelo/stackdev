78bdff08a303f2227d6b018bc1cafc55adbe1363
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-10-03 20:43:16 +0000

    Improve context renaming script
    fail if no posarg provided

--- mosk-dev-config-rename-context.sh
+++ mosk-dev-config-rename-context.sh
@@ -1,4 +1,8 @@
 in_file=$1
+if [ -z $in_file ]; then
+    echo need file to act on
+    exit 1
+fi
 new_name=${2:-$(basename $PWD)}
 old_name=$(kubectl --kubeconfig $in_file config get-contexts -oname)
 sed -i "s/: $old_name/: $new_name/" $in_file
