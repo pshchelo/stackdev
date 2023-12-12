dc8f6e286ae68eac35e89370ade324ea46d3e118
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-12-12 16:51:06 +0000

    Shellcheck-ify prepare-mosk-dev script
    aslo, add octavia-specific roles to created users

--- prepare-mosk-dev.sh
+++ prepare-mosk-dev.sh
@@ -4,17 +4,18 @@ pod=$(kubectl -n openstack get pod -l application=keystone,component=client -ojs
 # create separate admin user - not auto-rotated, with stable name and password, to be used from outside
 # run it from the keystone-client pod using the active admin creds there
 # NOTE: using system scope for Keystone ops as it is safest bet when strict admin / new policies are enforced
-kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system user create superadmin --domain Default --password superadmin --or-show
-kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --project admin --project-domain Default
-kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --domain Default
-osc_version=$(kubectl -n openstack exec $pod -c keystone-client -- openstack --version | awk '{print $2}')
+kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system user create superadmin --domain Default --password superadmin --or-show
+kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --project admin --project-domain Default
+kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --domain Default
+osc_version=$(kubectl -n openstack exec "$pod" -c keystone-client -- openstack --version | awk '{print $2}')
 # openstackclient supports assigning system roles since version 3.16.0/Rocky
-if $(echo -e "3.16.0\n$osc_version" | sort -V -C) ; then
-    kubectl -n openstack exec -ti $pod -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --system all
+if echo -e "3.16.0\n$osc_version" | sort -V -C ; then
+    kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --system all
 fi
 
 # execute the rest locally using separate admin user created above
 # needs access to public OpenStack API of MOSK dev cluster, e.g. sshuttle running
+# TODO: check that sshuttle is running or keystone.it.just.works is in /etc/hosts
 
 # custom MOSK role
 openstack --os-cloud mosk-dev-admin role add global-secret-decoder --user superadmin --user-domain Default --project admin --project-domain Default
@@ -26,13 +27,17 @@ openstack --os-cloud mosk-dev-admin project create demo --domain Default --or-sh
 # sandbox user to use w/o admin priveleges
 openstack --os-cloud mosk-dev-admin user create demo --domain Default --password demo --or-show
 openstack --os-cloud mosk-dev-admin role add member --user demo --user-domain Default --project demo --project-domain Default
+openstack --os-cloud mosk-dev-admin role add load-balancer_member --user demo --user-domain Default --project demo --project-domain Default
 openstack --os-cloud mosk-dev-admin role add creator --user demo --user-domain Default --project demo --project-domain Default
 # another sandbox user to use w/o admin privileges
 openstack --os-cloud mosk-dev-admin user create alt-demo --domain Default --password alt-demo --or-show
 openstack --os-cloud mosk-dev-admin role add member --user alt-demo --user-domain Default --project demo --project-domain Default
-# readonly user - reader role is present since Rocky
-if $(echo -e "3.16.0\n$osc_version" | sort -V -C) ; then
-    openstack --os-cloud mosk-dev-admin user create viewer --domain Default --password viewer
+# readonly user
+openstack --os-cloud mosk-dev-admin user create viewer --domain Default --password viewer
+openstack --os-cloud mosk-dev-admin role add load-balancer_observer --user viewer --user-domain Default --project demo --project-domain Default
+openstack --os-cloud mosk-dev-admin role add load-balancer_global_observer --user viewer --user-domain Default --project admin --project-domain Default
+# reader role is present since Rocky
+if echo -e "3.16.0\n$osc_version" | sort -V -C ; then
     openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project admin --project-domain Default
     openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project demo --project-domain Default
     openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --system all
@@ -53,5 +58,5 @@ for name in admin demo; do
     openstack --os-cloud mosk-dev-$name security group rule create $name --ingress --protocol tcp --dst-port 22 --description SSH
     openstack --os-cloud mosk-dev-$name security group rule create $name --ingress --protocol tcp --dst-port 80 --description HTPP
 
-    openstack --os-cloud mosk-dev-$name keypair create $name --public-key ${HOME}/.ssh/pub/aio_rsa.pub
+    openstack --os-cloud mosk-dev-$name keypair create $name --public-key "${HOME}/.ssh/pub/aio_rsa.pub"
 done
