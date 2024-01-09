a3165f8fdfc2f708a96e23b5fe836e95de187b42
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-01-09 17:14:12 +0200

    Handle real mcc when fetching deployed MOSK resources

--- mosk-dev-fetch-deployed-resources.sh
+++ mosk-dev-fetch-deployed-resources.sh
@@ -1,3 +1,6 @@
-kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator -oyaml > deployed.osctl.yaml
 kubectl -n openstack get osdpl osh-dev -oyaml > deployed.osdpl.yaml
 cp deployed.osdpl.yaml osdpl.yaml
+# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster
+if kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator ; then
+    kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator -oyaml > deployed.osctl.yaml
+fi
