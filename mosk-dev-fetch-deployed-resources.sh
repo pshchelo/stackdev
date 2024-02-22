e9d727a98bdef5bcccf1e6996ede89d208c35e14
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-02-22 13:29:51 +0200

    Account for MCC-deployed helmbundles

--- mosk-dev-fetch-deployed-resources.sh
+++ mosk-dev-fetch-deployed-resources.sh
@@ -1,6 +1,4 @@
 kubectl -n openstack get osdpl osh-dev -oyaml > deployed.osdpl.yaml
 cp deployed.osdpl.yaml osdpl.yaml
-# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster
-if kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator ; then
-    kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator -oyaml > deployed.osctl.yaml
-fi
+# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster, so ignore not found
+kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator --ignore-not-found=true -oyaml > deployed.osctl.yaml
