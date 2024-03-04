cb388722a32de5aad7cfc4cea07970e5044928b4
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-03-04 15:04:15 +0000

    Shellcheckify adapt-tempest-conf.sh
    and update README for it

--- tempest/README.md
+++ tempest/README.md
@@ -1,8 +1,18 @@
 # Running temtest tests locally
 
-1. Simulate proper tempest run by running nothing (replace 'script:' is OsDpl
+## Requirements
+- kubectl with context suited for listing pods and copying files from them
+- `crudini` tool for ini file modification
+
+## Procedure
+1. Install tempest and required plugins in a virtualenv, activate that venv.
+2. Simulate proper tempest run by running nothing (replace 'script:' is OsDpl
    with `sleep infinity`).
-2. Fetch the tempest config and blacklist file from the running tempest pod.
-3. Change some params in tempest.conf, see `adapt-tempest-conf.sh`.
-4. ?Something else?
-4. Run tempest as usual, with that config and blacklist file.
+   - this will generate all the required config files from OsDpl.
+3. Once tempest-run-test-... pod is running, run `adapt-tempest-conf.sh`
+   that will:
+  - fetch the tempest config and blacklist file from the running tempest pod
+  - fetch some additional things like images/binareis to use
+  - change some params in tempest.conf like paths and API urls
+  - generate a script for you to run with the above config and blacklist
+4. Run the generated sctipt, all passed arguments are passed to `tempest run`.
--- tempest/adapt-tempest-conf.sh
+++ tempest/adapt-tempest-conf.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 tempest_pod=$(kubectl -n openstack get pod -l application=tempest,component=run-tests -o jsonpath='{.items[*].metadata.name}')
 
-kubectl -n openstack cp -c tempest-run-tests $tempest_pod:/etc/tempest/test-blacklist ./test-blacklist
+kubectl -n openstack cp -c tempest-run-tests "$tempest_pod:/etc/tempest/test-blacklist" ./test-blacklist
 
 tempest_conf=${PWD}/tempest.conf
-kubectl -n openstack cp -c tempest-run-tests $tempest_pod:/etc/tempest/tempest.conf $tempest_conf
+kubectl -n openstack cp -c tempest-run-tests "$tempest_pod:/etc/tempest/tempest.conf" "$tempest_conf"
 
-crudini --del $tempest_conf identity ca_certificates_file
-crudini --set $tempest_conf identity disable_ssl_certificate_validation True
-crudini --set $tempest_conf identity v3_endpoint_type public
-crudini --set $tempest_conf identity uri_v3 https://keystone.it.just.works/v3
+crudini --del "$tempest_conf" identity ca_certificates_file
+crudini --set "$tempest_conf" identity disable_ssl_certificate_validation True
+crudini --set "$tempest_conf" identity v3_endpoint_type public
+crudini --set "$tempest_conf" identity uri_v3 https://keystone.it.just.works/v3
 
 resources_dir=${PWD}/resources
-mkdir -p $resources_dir
-crudini --set $tempest_conf DEFAULT state_path $resources_dir
+mkdir -p "$resources_dir"
+crudini --set "$tempest_conf" DEFAULT state_path "$resources_dir"
 
-kubectl -n openstack cp -c tempest-run-tests $tempest_pod:$(crudini --get $tempest_conf scenario img_file) $resources_dir/test.img
-crudini --set $tempest_conf scenario img_file $resources_dir/test.img
-kubectl -n openstack cp -c tempest-run-tests $tempest_pod:$(crudini --get $tempest_conf load_balancer test_server_path) $resources_dir/test_server.bin
-crudini --set $tempest_conf load_balancer test_server_path $resources_dir/test_server.bin
+remote_img_file_path=$(crudini --get "$tempest_conf" scenario img_file)
+kubectl -n openstack cp -c tempest-run-tests "$tempest_pod:$remote_img_file_path" "$resources_dir/test.img"
+crudini --set "$tempest_conf" scenario img_file "$resources_dir/test.img"
+
+remote_test_server_path=$(crudini --get "$tempest_conf" load_balancer test_server_path)
+kubectl -n openstack cp -c tempest-run-tests "$tempest_pod:$remote_test_server_path" "$resources_dir/test_server.bin"
+crudini --set "$tempest_conf" load_balancer test_server_path "$resources_dir/test_server.bin"
+
+remote_test_accounts_file=$(crudini --get "$tempest_conf" auth test_accounts_file)
+if [ -n "$remote_test_accounts_file" ]; then
+    kubectl -n openstack cp -c tempest-run-tests "$tempest_pod:$remote_test_accounts_file" "$resources_dir/static_accounts.yaml"
+    crudini --set "$tempest_conf" auth test_accounts_file "$resources_dir/static_accounts.yaml"
+fi
 
 cat > run-tempest.sh << EOF
 #!/usr/bin/env bash
