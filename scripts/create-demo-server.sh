bed6fc4fd38e7ae8dc5ed0f42ed944cd34861e8b
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-04-25 15:37:57 +0300

    Shellcheck-ify create-demo-server script

--- scripts/create-demo-server.sh
+++ scripts/create-demo-server.sh
@@ -3,29 +3,32 @@
 # NOTE: expects the project in use to have keypair, network and security group
 # named the same as project
 
-set -ex
+set -e
 server_name=$1
 server_flavor=${2:-m1.nano}
 server_image=${3:-Cirros-6.0}
 project=$(openstack configuration show -f value -c auth.project_name)
 script_dir=$(dirname "$0")
-openstack server create $server_name \
-    --key-name $project \
-    --network $project \
-    --security-group $project \
-    --image $server_image \
-    --flavor $server_flavor \
-    --user-data $script_dir/cirros-http-cpuload.userdata \
+openstack server create "$server_name" \
+    --key-name "$project" \
+    --network "$project" \
+    --security-group "$project" \
+    --image "$server_image" \
+    --flavor "$server_flavor" \
+    --user-data "$script_dir/cirros-http-cpuload.userdata" \
+    --use-config-drive \
     --wait
 
 # NOTE: admin sees all FIPs by default, but non-admin always get empty FIP list
 # when listing with project, even with their own, so need to differentiate
 project_filter=""
-if [ $project = "admin" ]; then
+if [ "$project" = "admin" ]; then
     project_filter="--project admin"
 fi
+
+# shellcheck disable=SC2086 # word splitting in project_filter is intentional
 fip=$(openstack floating ip list $project_filter --status DOWN -f value -c 'Floating IP Address' | shuf | head -n1)
-if [ -z $fip ]; then
+if [ -z "$fip" ]; then
     fip=$(openstack floating ip create public -f value -c floating_ip_address)
 fi
-openstack server add floating ip $1 $fip
+openstack server add floating ip "$1" "$fip"
