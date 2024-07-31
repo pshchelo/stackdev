ded44e1411b19165deb74f5b771daa262c31b2e9
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-07-31 13:01:48 +0300

    [mosk] update mosk-dev.sh wrapper
    add some error handling and feedback

--- mosk-dev.sh
+++ mosk-dev.sh
@@ -1,9 +1,12 @@
 # Working with MOSK virtual dev clusters
-#
-wget "https://mos-ci.infra.mirantis.net/job/deploy-k8s-ucp/$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
+if [ -z "$1" ]; then
+    echo "need deploy-openstack-k8s-env job number"
+    exit 1
+fi
+wget "https://mos-ci.infra.mirantis.net/job/deploy-openstack-k8s-env/$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
 ~/dotfiles/scripts/mosk/mosk-dev-config-rename-context.sh kubeconfig.yaml
 ~/dotfiles/scripts/mosk/mosk-dev-fetch-deployed-resources.sh
 ~/dotfiles/scripts/k8s/k8s-cleanup-pods.sh -A
+# the next command needs mosk-dev-connect running,
+# but it will pause if that is not running yet
 ~/dotfiles/scripts/mosk/mosk-dev-create-resources.sh
-# in a separate window startmosk-dev-connect
-# ~/dotfiles/script/mosk/mosk-dev-connect
