3c72f6a72d7f35d542dba2929a7db27fd840d36c
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-08-08 12:17:32 +0300

    Improve solitary-servers script
    add proper argparse handling
    add header row to output
    add flavor info to the output

--- scripts/limits/solitary-servers.py
+++ scripts/limits/solitary-servers.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
+import argparse
+
 import openstack
 
-cloud = openstack.connect()
+parser = argparse.ArgumentParser(
+    prog="solitary-servers",
+    description="Find Nova servers that are not part of any Heat Stack"
+)
+parser.add_argument(
+    "--os-cloud",
+    dest="cloud_name",
+    help="Cloud to use (from clouds.yaml). Default is from OS_CLOUD env var."
+)
+args = parser.parse_args()
 
+cloud = openstack.connect(cloud=args.cloud_name)
 
 def get_servers_in_stacks():
-    heat = cloud.orchestration
     servers_in_stacks = []
-    for stack in heat.stacks():
-        server_resources = heat.get(
+    for stack in cloud.orchestration.stacks():
+        server_resources = cloud.orchestration.get(
             f"/stacks/{stack.name}/{stack.id}/resources",
             params={"type": "OS::Nova::Server", "nested_depth": "999"}
         ).json()
@@ -18,11 +29,31 @@ def get_servers_in_stacks():
 
 
 def get_solitary_servers():
-    nova = cloud.compute
-    servers_in_stacks = get_servers_in_stacks()
-    return [s for s in nova.servers() if s.id not in servers_in_stacks]
+    return [
+        s for s in cloud.compute.servers()
+        if s.id not in get_servers_in_stacks()
+    ]
 
 
 if __name__ == "__main__":
+    print(
+        "Instance-Id",
+        "Created-At",
+        "Status",
+        "Task-State",
+        "VCPU",
+        "RAM",
+        "Total-Disk",
+        "Name",
+    )
     for server in sorted(get_solitary_servers(), key=lambda x: x.created_at):
-        print(server.id, server.created_at, server.status, server.name)
+        print(
+            server.id,
+            server.created_at,
+            server.status,
+            server.task_state,
+            server.flavor.vcpus,
+            server.flavor.ram,
+            server.flavor.disk + server.flavor.swap + server.flavor.ephemeral,
+            server.name,
+        )
