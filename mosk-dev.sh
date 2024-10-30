7fc172cca4574b5752568251fa2a68cf4ab49840
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-10-30 12:37:28 +0200

    Require full job url in mosk-dev.sh script

--- mosk-dev.sh
+++ mosk-dev.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+set -e
 # Preparations to work with fresh MOSK virtual dev cluster
 RED='\033[0;31m'
 NOC='\033[0m'
 
 if [ -z "$1" ]; then
-    echo "need deploy-openstack-k8s-env job number"
+    echo "need URL to deploy job with 'kubeconfig-child-cluster.yml' artifact"
     exit 1
 fi
 echo "Get kubeconfig for the env"
-wget "https://mos-ci.infra.mirantis.net/job/deploy-openstack-k8s-env/$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
+wget "$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
 echo "Edit kubeconfig for the env"
 ~/dotfiles/scripts/mosk/mosk-dev-config-rename-context.sh kubeconfig.yaml
 echo -e "${RED}START mosk-dev-connect in a separate shell NOW${NOC}"
