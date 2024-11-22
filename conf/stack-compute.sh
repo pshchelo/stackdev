04d640eb185add107a0c22ea8121869fce3c5929
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-11-22 12:20:14 +0000

    Update and rename script for devstack compute
    now auto-detects the address of controller using local.conf

---
+++ conf/stack-compute.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+STACK_DIR="/opt/stack"
+DEVSTACK_DIR="$STACK_DIR/devstack"
+STACK_DATA_DIR="$STACK_DIR/data"
+
+eval "$(grep ^SERVICE_HOST $DEVSTACK_DIR/local.conf)"
+
+mkdir -p $STACK_DATA_DIR
+scp -r "$SERVICE_HOST:/$STACK_DATA_DIR/CA"  $STACK_DATA_DIR
+scp "$SERVICE_HOST:/$STACK_DATA_DIR/*.pem" $STACK_DATA_DIR
+
+pushd $DEVSTACK_DIR
+./stack.sh
+popd
+
+ssh "${SERVICE_HOST}" -- $DEVSTACK_DIR/tools/discover_hosts.sh
