8a6c2bee7163a5658df2ab3da42def3507f0c66b
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-11-28 19:36:00 +0200

    Create .envrc file in mosk-dev.sh

--- mosk-dev.sh
+++ mosk-dev.sh
@@ -12,7 +12,14 @@ echo "Get kubeconfig for the env"
 wget "$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
 echo "Edit kubeconfig for the env"
 ~/dotfiles/scripts/mosk/mosk-dev-config-rename-context.sh kubeconfig.yaml
+cat > .envrc << EOF
+export KUBECONFIG="$PWD/kubeconfig.yaml"
+export OS_CLOUD=mosk-dev-admin
+EOF
+direnv allow
+source .envrc
 echo -e "${RED}START mosk-dev-connect in a separate shell NOW${NOC}"
+sleep 5
 echo "Create local copies of deployed resources"
 ~/dotfiles/scripts/mosk/mosk-dev-fetch-deployed-resources.sh
 echo "Cleanup 'complete' pods"
