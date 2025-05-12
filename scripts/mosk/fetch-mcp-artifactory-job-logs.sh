665744457e8a1d0e166739f9528f0478b3352830
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-05-12 13:04:39 +0000

    Improve artifactory log fetching script
    make missing argument error more clear

--- scripts/mosk/fetch-mcp-artifactory-job-logs.sh
+++ scripts/mosk/fetch-mcp-artifactory-job-logs.sh
@@ -2,12 +2,12 @@
 ARTIFACTORY_URL=${ARTIFACTORY_URL:-https://artifactory.mcp.mirantis.net/artifactory/oscore-local/mcp2}
 JOB=$1
 if [ -z "$JOB" ]; then
-    echo need job name to download for
+    echo "need job name to download logs for: $ARTIFACTORY_URL/<JOB NAME>"
     exit 1
 fi
 wget --mirror \
      --no-host-directories \
      --no-parent \
      --cut-dirs 3 \
-     --reject="index.htm*" \
+     --reject='index.htm*' \
      "$ARTIFACTORY_URL/$JOB/"
