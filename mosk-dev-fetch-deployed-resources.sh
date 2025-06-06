2c937d74f34a9b41e549d93cc21e90d41bffef28
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-06-06 16:13:15 +0300

    Fix mosk-dev-fetch-deployed-resources
    return 0 on success

--- mosk-dev-fetch-deployed-resources.sh
+++ mosk-dev-fetch-deployed-resources.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 kubectl -n openstack get osdpl osh-dev -oyaml > deployed.osdpl.yaml
-# TODO: automate cleanup of osdpl yaml - remove status and all metadata except name and namespace
+# TODO: automate cleanup of osdpl yaml
+# remove status and all metadata except name and namespace
 cp deployed.osdpl.yaml osdpl.yaml
-# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster, so ignore not found
-kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator --ignore-not-found=true -oyaml > deployed.osctl.yaml
-[ ! -s deployed.osctl.yaml ] && rm deployed.osctl.yaml
+# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster, 
+# so ignore not found and delete empty file
+kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator \
+    --ignore-not-found=true -oyaml > deployed.osctl.yaml
+if [ ! -s deployed.osctl.yaml ]; then
+    rm deployed.osctl.yaml
+fi
