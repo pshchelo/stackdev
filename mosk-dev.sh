2724fd86ebbcbe5fd4819e8a4f57ffa02f0c5766
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-06-06 13:02:19 +0000

    Add read-only mode to mosk-dev.sh

--- mosk-dev.sh
+++ mosk-dev.sh
@@ -3,13 +3,33 @@ set -e
 # Preparations to work with fresh MOSK virtual dev cluster
 RED='\033[0;31m'
 NOC='\033[0m'
+READONLY=0
+SCRIPT_NAME=$(basename "$0")
+__usage="
+Usage: $SCRIPT_NAME [-r] <URL>
+Prepare (virtual) MOSK development env for my custom settings:
+- fetch and massage kubeconfig
+- create local copies of main MOSK k8s resources like OsDpl
+- create test user accounts, networks, flavors etc
 
-if [ -z "$1" ]; then
-    echo "need URL to deploy job with 'kubeconfig-child-cluster.yml' artifact"
+<URL> URL to deploy job with 'kubeconfig-child-cluster.yml' artifact
+-r    read-only mode, do not create test user accounts, networks etc
+"
+while getopts ':hr' arg; do
+    case "$arg" in
+        r) READONLY=1 ;;
+        h) echo "$__usage"; exit 0 ;;
+        *) echo "$__usage"; exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+url=$1
+if [ -z "$url" ]; then
+    echo "$__usage"
     exit 1
 fi
 echo "Get kubeconfig for the env"
-wget "$1/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
+wget "$url/artifact/kubeconfig-child-cluster.yml" -O kubeconfig.yaml
 echo "Edit kubeconfig for the env"
 ~/dotfiles/scripts/mosk/mosk-dev-config-rename-context.sh kubeconfig.yaml
 cat > .envrc << EOF
@@ -18,11 +38,13 @@ export OS_CLOUD="mosk-dev-admin"
 EOF
 direnv allow
 source .envrc
-echo -e "${RED}START mosk-dev-connect in a separate shell NOW${NOC}"
-sleep 5
 echo "Create local copies of deployed resources"
 ~/dotfiles/scripts/mosk/mosk-dev-fetch-deployed-resources.sh
-# the next command needs mosk-dev-connect running,
-# but it will pause if that is not running yet
-echo "Create my default test env users and infra"
-~/dotfiles/scripts/mosk/mosk-dev-create-resources.sh
+if [ $READONLY -eq 0 ]; then
+    echo -e "${RED}START mosk-dev-connect in a separate shell NOW${NOC}"
+    sleep 5
+    # the next command needs mosk-dev-connect running,
+    # but it will pause if that is not running yet
+    echo "Create my default test env users and infra"
+    ~/dotfiles/scripts/mosk/mosk-dev-create-resources.sh
+fi
