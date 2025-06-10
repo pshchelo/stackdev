d3d126b052591ca3cf7f6626e17b8c0dae787d57
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-06-10 12:40:56 +0300

    Create viewer mosk-dev user idempotently

--- mosk-dev-create-resources.sh
+++ mosk-dev-create-resources.sh
@@ -15,7 +15,7 @@ echo "resolving openstackclient version"
 # create separate admin user - not auto-rotated, with stable name and password, to be used from outside
 # run it from the keystone-client pod using the active admin creds there
 # NOTE: using system scope for Keystone ops as it is safest bet when strict admin / new policies are enforced
-echo "creating superadmin user"
+echo "ensuring superadmin user exists"
 kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system user create --or-show superadmin --domain Default --password superadmin
 kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --project admin --project-domain Default
 kubectl -n openstack exec -ti "$pod" -c keystone-client -- openstack --os-cloud admin-system role add admin --user superadmin --user-domain Default --domain Default
@@ -29,7 +29,7 @@ fi
 
 # make superadmin also an admin in system scope
 openstack --os-cloud mosk-dev-admin role add admin --user superadmin --user-domain Default --system all
-echo "creating required roles"
+echo "ensuring required roles exist"
 # this is MOSK-specific role
 openstack --os-cloud mosk-dev-admin role create --or-show global-secret-decoder
 openstack --os-cloud mosk-dev-admin role add global-secret-decoder --user superadmin --user-domain Default --project admin --project-domain Default
@@ -44,27 +44,28 @@ openstack --os-cloud mosk-dev-admin role create --or-show load-balancer_global_o
 openstack --os-cloud mosk-dev-admin role create --or-show reader
 
 # sandbox project to use w/o admin privileges
-echo "creating sandbox project"
+echo "ensuring sandbox project exists"
 openstack --os-cloud mosk-dev-admin project create --or-show demo --domain Default
 # sandbox user to use w/o admin priveleges
-echo "creating user demo"
+echo "ensuring user demo exists"
 openstack --os-cloud mosk-dev-admin user create --or-show demo --domain Default --password demo
 openstack --os-cloud mosk-dev-admin role add member --user demo --user-domain Default --project demo --project-domain Default
 openstack --os-cloud mosk-dev-admin role add load-balancer_member --user demo --user-domain Default --project demo --project-domain Default
 openstack --os-cloud mosk-dev-admin role add creator --user demo --user-domain Default --project demo --project-domain Default
 # another sandbox user to use w/o admin privileges
-echo "creating user alt-demo"
+echo "ensuring user alt-demo exists"
 openstack --os-cloud mosk-dev-admin user create --or-show alt-demo --domain Default --password alt-demo
 openstack --os-cloud mosk-dev-admin role add member --user alt-demo --user-domain Default --project demo --project-domain Default
 # readonly user
-echo "creating read-only user viewer"
-openstack --os-cloud mosk-dev-admin user create viewer --domain Default --password viewer
+echo "ensuring read-only user viewer exists"
+openstack --os-cloud mosk-dev-admin user create --or-show viewer --domain Default --password viewer
 openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project admin --project-domain Default
 openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --project demo --project-domain Default
 openstack --os-cloud mosk-dev-admin role add reader --user viewer --user-domain Default --system all
 openstack --os-cloud mosk-dev-admin role add load-balancer_observer --user viewer --user-domain Default --project demo --project-domain Default
 openstack --os-cloud mosk-dev-admin role add load-balancer_global_observer --user viewer --user-domain Default --project admin --project-domain Default
 
+# TODO: make everything below idempotent, create -> ensure
 # Minimal flavor for cirros
 echo "creating minimal flavor"
 openstack --os-cloud mosk-dev-admin flavor create m1.nano --ram 128 --disk 1 --vcpus 1
