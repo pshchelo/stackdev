8eebea03d877f7eb331a465fa019e12ad4cf5c35
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-08-26 15:42:35 +0000

    [mosk] fetch or create a stub of osdpl artifacts cm

--- mosk-dev-fetch-deployed-resources.sh
+++ mosk-dev-fetch-deployed-resources.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
-kubectl -n openstack get osdpl osh-dev -oyaml > deployed.osdpl.yaml
+# rely on fact that there's ever only one OsDpl object in the MOSK cluster
+OSDPL_NAME=$(kubectl -n openstack get osdpl \
+    -o jsonpath='{.items[].metadata.name}')
+kubectl -n openstack get osdpl "$OSDPL_NAME" -oyaml > deployed.osdpl.yaml
 # TODO: automate cleanup of osdpl yaml
 # remove status and all metadata except name and namespace
 cp deployed.osdpl.yaml osdpl.yaml
-# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster, 
+# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster,
 # so ignore not found and delete empty file
 kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator \
     --ignore-not-found=true -oyaml > deployed.osctl.yaml
 if [ ! -s deployed.osctl.yaml ]; then
     rm deployed.osctl.yaml
+fi
+# fetch or create a stub for OsDpl artifacts config map
+kubectl -n openstack get cm "${OSDPL_NAME}-artifacts" \
+    --ignore-not-found -o yaml > osdpl-artifacts.yaml
+if [ ! -s osdpl-artifacts.yaml ]; then
+    # resolve openstack release
+    OS_RELEASE=$(kubectl -n openstack get osdpl "$OSDPL_NAME" \
+        -o jsonpath='{.spec.openstack_version}')
+    # fill in a stub
+    cat > osdpl-artifacts.yaml << EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  labels:
+    openstack.lcm.mirantis.com/watch: "true"
+  name: ${OSDPL_NAME}-artifacts
+  namespace: openstack
+data:
+  ${OS_RELEASE}: |
+EOF
 fi
