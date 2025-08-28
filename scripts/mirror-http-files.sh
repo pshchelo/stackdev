38748e543cd6e0f06cb480724e0f70778ebcd4c0
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-08-28 13:43:04 +0000

    Auto-detect cut-dirs value in mirror-http-files

--- scripts/mirror-http-files.sh
+++ scripts/mirror-http-files.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # downloads whole site ignoring index.htm[l] files
-URL=$1
+URL=${1%/} # strip trailing slash so that counting CUT_DIRS is deterministic
 if [ -z "${URL}" ]; then
     echo "need URL to mirror files from"
     exit 1
 fi
+slashes="${URL//[^\/]}"  # leave only slashes from URL
+num_slashes="${#slashes}" # count them
+CUT_DIRS=$(( num_slashes - 3 )) # account for schema and hostname slashes
+
 # TODO: accept regexes for --reject as input
-# TODO: infer CUT_DIRS from URL
-CUT_DIRS=3 # https://artifactory.mcp.mirantis.net/artifactory/oscore-local/mcp2
 wget --mirror \
      --no-host-directories \
      --no-parent \
