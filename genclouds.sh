5eb6a3b9c110930ce65fdf25d8445a8cf07d08d6
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-11-04 19:23:23 +0200

    Autodiscover domain in genclouds.sh

--- genclouds.sh
+++ genclouds.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 # generate clouds.yaml with admin account for the test MOS env
-# TODO: discover URL from osdpl
-URL=${1:-"https://keystone.it.just.works"}
+svcdomain=$(kubectl -n openstack get osdpl -ojsonpath='{.items[0].spec.public_domain_name}')
 clouds_yaml=$(kubectl -n openstack get secret keystone-os-clouds -ojsonpath='{.data.clouds\.yaml}' | base64 -d)
 cat > clouds.yaml << EOF
 clouds:
   admin:
     auth:
-      auth_url: $URL
+      auth_url: https://keystone.${svcdomain}
       username: $(echo  "$clouds_yaml" | yq -r .clouds.admin.auth.username)
       password: $(echo  "$clouds_yaml" | yq -r .clouds.admin.auth.password)
       project_name: admin
