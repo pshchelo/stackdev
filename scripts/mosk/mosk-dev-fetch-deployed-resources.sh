709f986e4b0237777f75703b2db4a4bb950d1650
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-11-27 12:43:34 +0000

    Unhardcode file paths in fetch mosk resourecs script

--- scripts/mosk/mosk-dev-fetch-deployed-resources.sh
+++ scripts/mosk/mosk-dev-fetch-deployed-resources.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 # rely on fact that there's ever only one OsDpl object in the MOSK cluster
+OSDPL_FILE="osdpl.yaml"
+OSCTL_FILE="osctl.yaml"
+IMAGES_FILE="images-cm.yaml"
+DEPLOYED_OSDPL_FILE="deployed.$OSDPL_FILE"
+DEPLOYED_OSCTL_FILE="deployed.$OSCTL_FILE"
+DEPLOYED_IMAGES_FILE="deployed.$IMAGES_FILE"
 OSDPL_NAME=$(kubectl -n openstack get osdpl \
     -o jsonpath='{.items[].metadata.name}')
-kubectl -n openstack get osdpl "$OSDPL_NAME" -oyaml > deployed.osdpl.yaml
+kubectl -n openstack get osdpl "$OSDPL_NAME" -oyaml > "$DEPLOYED_OSDPL_FILE"
 # TODO: automate cleanup of osdpl yaml
 # remove status and all metadata except name and namespace
-cp deployed.osdpl.yaml osdpl.yaml
-# mcc-deployed envs have the helmbundle for the operator in the mgmt cluster,
-# so ignore not found and delete empty file
+cp "$DEPLOYED_OSDPL_FILE" "$OSDPL_FILE"
 kubectl -n osh-system get helmbundles.lcm.mirantis.com openstack-operator \
-    --ignore-not-found=true -oyaml > deployed.osctl.yaml
-if [ ! -s deployed.osctl.yaml ]; then
-    rm deployed.osctl.yaml
+    --ignore-not-found=true -oyaml > "$DEPLOYED_OSCTL_FILE"
+if [ ! -s "$DEPLOYED_OSCTL_FILE" ]; then
+    cp "$DEPLOYED_OSCTL_FILE" "$OSCTL_FILE"
+else
+    # mcc-deployed envs have the helmbundle for the operator
+    # in the mgmt cluster, so ignore not found and delete empty file
+    rm "$DEPLOYED_OSCTL_FILE"
 fi
 # fetch or create a stub for OsDpl artifacts config map
 kubectl -n openstack get cm "${OSDPL_NAME}-artifacts" \
-    --ignore-not-found -o yaml > osdpl-artifacts.yaml
-if [ ! -s osdpl-artifacts.yaml ]; then
+    --ignore-not-found -o yaml > "$DEPLOYED_IMAGES_FILE"
+if [ -s  "$DEPLOYED_IMAGES_FILE" ]; then
+    cp "$DEPLOYED_IMAGES_FILE" "$IMAGES_FILE"
+else
+    rm "$DEPLOYED_IMAGES_FILE"
     # resolve openstack release
     OS_RELEASE=$(kubectl -n openstack get osdpl "$OSDPL_NAME" \
         -o jsonpath='{.spec.openstack_version}')
     # fill in a stub
-    cat > osdpl-artifacts.yaml << EOF
+    cat > "$IMAGES_FILE" << EOF
 apiVersion: v1
 kind: ConfigMap
 metadata:
