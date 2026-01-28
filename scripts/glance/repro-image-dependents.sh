7cc7a4ccb592bdfa7c710072acefb03587d3cbba
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2026-01-28 10:55:43 +0000

    Fix output of rbd image depenencies test script

--- scripts/glance/repro-image-dependents.sh
+++ scripts/glance/repro-image-dependents.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-dd if=/dev/random of=/tmp/random1k bs=1024 count=1
-image_id=$(openstack image create random --file /tmp/random1k --disk-format raw --container-format bare --public -f value -c id)
+set -e
+name="random"
+dd if=/dev/random of=/tmp/random1k bs=1024 count=1 status=none
+image_id=$(openstack image create "$name" --file /tmp/random1k --disk-format raw --container-format bare --public -f value -c id)
 rm /tmp/random1k
-echo Image: "$image_id"
-server_id=$(openstack server create --image "$image_id" --flavor m1.nano --no-network --wait random)
-echo Server: "$server_id"
+echo Image: "$name" "$image_id"
+server_id=$(openstack server create "$name" --image "$image_id" --flavor m1.nano --no-network --wait -f value -c id)
+echo Server: "$name" "$server_id"
