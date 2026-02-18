8daaa9957b8bc53d14505c36c9ee5aff16841668
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2026-02-18 17:05:08 +0000

    Expand user for sitests inventory
    ansible does that by itself, but sitests not.

--- scripts/heat-stack-inventory.py
+++ scripts/heat-stack-inventory.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 """
 Generate YAML with nodes info usable as Ansible inventory (default)
 or with Mirantis si-tests harness. Output is printed to stdout.
 """
+
 import argparse
 import openstack
 import os
@@ -11,12 +13,16 @@ import yaml
 
 def get_server_fips_from_stack(
     cloud: openstack.connection.Connection,
-    stack_name: str, ip_version: int = 4
-) -> dict[str: dict[str, str]]:
+    stack_name: str,
+    ip_version: int = 4,
+) -> dict[str : dict[str, str]]:
     stack = cloud.orchestration.find_stack(stack_name)
     server_resources = cloud.orchestration.get(
         f"/stacks/{stack.name}/{stack.id}/resources",
-        params={"type": "OS::Nova::Server", "nested_depth": "999"}
+        params={
+            "type": "OS::Nova::Server",
+            "nested_depth": "999",
+        },
     ).json()
     servers = {}
     for r in server_resources["resources"]:
@@ -29,24 +35,29 @@ def get_server_fips_from_stack(
         for net_addrs in server.addresses.values():
             for addr in net_addrs:
                 if (
-                    addr["OS-EXT-IPS:type"] == "floating" and
-                    addr['version'] == ip_version
+                    addr["OS-EXT-IPS:type"] == "floating"
+                    and addr["version"] == ip_version
                 ):
-                        server_fip = addr["addr"]
-                        break
+                    server_fip = addr["addr"]
+                    break
             if server_fip:
                 servers[server.hostname] = server_fip
                 break
         else:
-            print(f"WARNING: no FIP with IPv{ip_version} found "
-                  f"for server {server_id}")
+            print(
+                f"WARNING: no FIP with IPv{ip_version} found "
+                f"for server {server_id}"
+            )
     return {stack_name: servers}
 
 
 def format_ansible(
-    servers: dict[str: dict[str: str]],
-    user: str, key: str, port: int = 22, strict: bool = True
-) -> dict[str: dict]:
+    servers: dict[str : dict[str:str]],
+    user: str,
+    key: str,
+    port: int = 22,
+    strict: bool = True,
+) -> dict[str:dict]:
     inventory = {}
     for group, srvs in servers.items():
         ssh_opts = []
@@ -56,28 +67,36 @@ def format_ansible(
             "ansible_port": port,
         }
         if not strict:
-            ssh_opts.extend([
-                "-o StrictHostKeyChecking=no",
-                "-o UserKnownHostsFile=/dev/null",
-            ])
+            ssh_opts.extend(
+                [
+                    "-o StrictHostKeyChecking=no",
+                    "-o UserKnownHostsFile=/dev/null",
+                ]
+            )
         if ssh_opts:
             vars["ansible_ssh_common_args"] = " ".join(ssh_opts)
         hosts = {name: {"ansible_host": ip} for name, ip in srvs.items()}
-        inventory[group] = {"vars": vars, "hosts": hosts}
+        inventory[group] = {
+            "vars": vars,
+            "hosts": hosts,
+        }
     return inventory
 
 
 def format_sitests(
-    servers: dict[str: dict[str: str]],
-    user: str, key: str, port: int = 22, strict: bool = True
-) -> dict[str: dict]:
+    servers: dict[str : dict[str:str]],
+    user: str,
+    key: str,
+    port: int = 22,
+    strict: bool = True,
+) -> dict[str:dict]:
     inventory = {}
     for server, address in list(servers.values())[0].items():
         inventory[server] = {"ip": {"address": address}}
         inventory[server]["ssh"] = {
-                "username": user,
-                "private_key_path": key,
-                "port": port,
+            "username": user,
+            "private_key_path": os.path.expanduser(key),
+            "port": port,
         }
     return inventory
 
@@ -89,28 +108,60 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=this_module.__doc__,
     )
-    parser.add_argument("stack", help="Name or ID of the stack to parse")
-    parser.add_argument("--user", default="ubuntu", help="SSH user to use")
-    parser.add_argument("--key", default="~/.ssh/aio_rsa",
-                        help="path to SSH private key")
-    parser.add_argument("--port", default=22, help="SSH port to use")
-    parser.add_argument("--ip-version", type=int, choices=[4, 6], default=4,
-                        help="IP version of address to use")
-    parser.add_argument("--format",
-                        choices=["ansible", "sitests"],
-                        default="ansible",
-                        help="Format of inventory to generate")
-    parser.add_argument("--strict", action="store_true",
-                        help="Enable strict host key check")
+    parser.add_argument(
+        "stack",
+        help="Name or ID of the stack to parse",
+    )
+    parser.add_argument(
+        "--user",
+        default="ubuntu",
+        help="SSH user to use",
+    )
+    parser.add_argument(
+        "--key",
+        default="~/.ssh/aio_rsa",
+        help="path to SSH private key",
+    )
+    parser.add_argument(
+        "--port",
+        default=22,
+        help="SSH port to use",
+    )
+    parser.add_argument(
+        "--ip-version",
+        type=int,
+        choices=[4, 6],
+        default=4,
+        help="IP version of address to use",
+    )
+    parser.add_argument(
+        "--format",
+        choices=["ansible", "sitests"],
+        default="ansible",
+        help="Format of inventory to generate",
+    )
+    parser.add_argument(
+        "--strict",
+        action="store_true",
+        help="Enable strict host key check",
+    )
     cloud = openstack.connect(options=parser)
     args = parser.parse_args()
     formatter = getattr(this_module, f"format_{args.format}", None)
     if not formatter:
         raise NotImplementedError(f"No formatter for {args.format} found.")
     servers = get_server_fips_from_stack(
-        cloud, args.stack, ip_version=args.ip_version)
+        cloud,
+        args.stack,
+        ip_version=args.ip_version,
+    )
     inventory = formatter(
-        servers, args.user, args.key, port=args.port, strict=args.strict)
+        servers,
+        args.user,
+        args.key,
+        port=args.port,
+        strict=args.strict,
+    )
     print(yaml.dump(inventory))
 
 
