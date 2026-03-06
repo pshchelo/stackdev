201ef7b134489360379d912e179f33765d9377ed
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2026-03-06 12:54:11 +0000

    Fix argument parsing in run-tests-in-docker

--- ci/run-tests-in-docker.sh
+++ ci/run-tests-in-docker.sh
@@ -5,23 +5,34 @@ image_path="mirantis/openstack-ci/openstack-ci-python3-test"
 image_tag="noble"
 UPPER_CONSTRAINTS_FILE=${UPPER_CONSTRAINTS_FILE:-"$HOME/src/openstack/requirements/upper-constraints.txt"}
 DEBUG=0
+TOX_VER=""
+VENV_VER=""
 
 function get_help {
     local script_name
     script_name=$(basename "$0")
-    echo "Usage: $script_name TOX_ENV [-r <docker repo url>] [-i <docker image path>] [-t <docker image tag>] [-h] [-v]"
+    echo "Usage: $script_name TOX_ENV [-r <docker repo url>] [-i <docker image path>] [-t <docker image tag>] [-x <tox version specifier>] [-e <virtualenv version specifier>] [-h] [-v] "
     echo "defaults are:"
     echo "<docker repo url> $image_repo"
     echo "<docker image path> $image_path"
     echo "<docker image tag> $image_tag"
 }
 
-while getopts ':r:i:t:hv' arg; do
+tox_env=$1
+if [ -z "$tox_env" ]; then
+    get_help
+    exit 1
+fi
+shift
+
+while getopts ':r:i:t:x:e:hv' arg; do
     case "${arg}" in
         r) image_repo="${OPTARG}" ;;
         i) image_path="${OPTARG}" ;;
         t) image_tag="${OPTARG}" ;;
         v) DEBUG=1 ;;
+        x) TOX_VER="${OPTARG}" ;;
+        e) VENV_VER="${OPTARG}" ;;
         h) get_help ;;
         ?) get_help; exit 1 ;;
     esac
@@ -31,17 +42,27 @@ if [[ $DEBUG -eq 1 ]]; then
     set -x
 fi
 
-tox_env=${*:$OPTIND:1}
-if [ -z "$tox_env" ]; then
-    get_help
-    exit 1
-fi
-
 name="mcp-ci-$tox_env"
 image="$image_repo/$image_path:$image_tag"
 # This is how it is started on CI, in this example for python-openstackclient
 # workspace dir has both 'python-openstackclient' and 'requirements' dirs
-#docker run -d -t --group-add jenkins --group-add 1001 -e LC_ALL=en_US.UTF-8 -e TOX_ENV=py38 -e VIRTUALENV_VER= -e WORKSPACE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal -w /var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/python-openstackclient -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/jenkins/workspace/yoga-openstack-test-py38-focal:/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal --sysctl net.ipv6.conf.all.disable_ipv6=0 --name jenkins-yoga-openstack-test-py38-focal-592 -e UPPER_CONSTRAINTS_FILE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/requirements/upper-constraints.txt -e TOX_CONSTRAINTS_FILE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/requirements/upper-constraints.txt docker-dev-virtual.docker.mirantis.net/mirantis/openstack-ci/openstack-ci-python3-test:focal /bin/cat
+#docker run -d -t \
+#    --group-add jenkins \
+#    --group-add 1001 \
+#    -e LC_ALL=en_US.UTF-8 \
+#    -e TOX_ENV=py38 \
+#    -e VIRTUALENV_VER= \
+#    -e TOX_VER=<4.0.0
+#    -e UPPER_CONSTRAINTS_FILE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/requirements/upper-constraints.txt \
+#    -e TOX_CONSTRAINTS_FILE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/requirements/upper-constraints.txt \
+#    -e WORKSPACE=/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal \
+#    -w /var/lib/jenkins/workspace/yoga-openstack-test-py38-focal/python-openstackclient \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    -v /var/lib/jenkins/workspace/yoga-openstack-test-py38-focal:/var/lib/jenkins/workspace/yoga-openstack-test-py38-focal \
+#    --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+#    --name jenkins-yoga-openstack-test-py38-focal-592 \
+#    docker-dev-virtual.docker.mirantis.net/mirantis/openstack-ci/openstack-ci-python3-test:focal \
+#    /bin/cat
 
 docker_args="-d -t"
 docker_args+=" --name $name"
@@ -49,6 +70,8 @@ docker_args+=" --group-add jenkins"
 docker_args+=" --group-add 1000"
 docker_args+=" -e LC_ALL=en_US.UTF-8"
 docker_args+=" -e TOX_ENV=$tox_env"
+docker_args+=" -e TOX_VER=$TOX_VER"
+docker_args+=" -e VIRTUALENV_VER=$VENV_VER"
 docker_args+=" -e UPPER_CONSTRAINTS_FILE=/opt/upper-constraints.txt"
 docker_args+=" -e TOX_CONSTRAINTS_FILE=/opt/upper-constraints.txt"
 docker_args+=" -e WORKSPACE=/opt/project"
