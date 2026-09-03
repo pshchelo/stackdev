8a847b18d5cab4f7ac769d10225194eecd4e282b
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2026-09-03 17:22:03 +0300

    Use wget2 if available for multi-threaded mirriring.

--- scripts/mirror-http-files.sh
+++ scripts/mirror-http-files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # downloads whole site ignoring index.htm[l] files
 URL=${1%/} # strip trailing slash so that counting CUT_DIRS is deterministic
-URL=${URL//\/ui\/native\//\/artifactory\/} # mangle Artifactory browser URL into artifactory download URL
+URL=${URL//\/ui\/native\//\/artifactory\/} # convert Artifactory UI browser URL into artifactory download URL
 if [ -z "${URL}" ]; then
     echo "need URL to mirror files from"
     exit 1
@@ -10,10 +10,20 @@ slashes="${URL//[^\/]}"  # leave only slashes from URL
 num_slashes="${#slashes}" # count them
 CUT_DIRS=$(( num_slashes - 3 )) # account for schema and hostname slashes
 
+#TODO: autodetect default as half available cores
+NUM_THREADS=${2:-4}
+
+downloader="wget"
+download_opts=" --mirror "
+download_opts+=" --no-host-directories "
+download_opts+=" --no-parent "
+download_opts+=" --cut-dirs ${CUT_DIRS} "
 # TODO: accept regexes for --reject as input
-wget --mirror \
-     --no-host-directories \
-     --no-parent \
-     --cut-dirs ${CUT_DIRS} \
-     --reject='index.htm*' \
-     "${URL}/"
+download_opts+=" --reject 'index.htm*' "
+if command wget2 > /dev/null 2>&1; then
+    downloader="wget2"
+    download_opts+=" --max-threads ${NUM_THREADS}"
+fi
+
+# shellcheck disable=SC2086 # word split of $download_opts is intentional
+${downloader} ${download_opts} "${URL}/"
